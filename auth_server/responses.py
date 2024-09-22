0c24b90cd66db636aa7465593ad0900e7188db08
eduardo <eduz@MacBook-Pro-di-edutardo.local>
2024-09-22 13:52:51 +0200

    ancora setup

--- auth_server/responses.py
+++ auth_server/responses.py
@@ -14,3 +14,49 @@ def bad_request(err, descr):
             "Pragma": "no-cache"
         }
     )
+def temporarily_unavailable(descr):
+    return Response(
+        response=dumps({
+            'error': 'temporarily_unavailable',
+            'error_description': descr
+        }),
+        status=503,
+        mimetype='application/json',
+        headers={
+            "Cache-Control": "no-store",
+            "Pragma": "no-cache"
+        }
+    )
+def server_error():
+    return Response(
+        response=dumps({
+            'error': 'server_error',
+            'error_description': 'Internal server error'
+        }),
+        status=500,
+        mimetype='application/json',
+        headers={
+            "Cache-Control": "no-store",
+            "Pragma": "no-cache"
+        }
+    )
+def forbidden(descr):
+    return Response(
+        response=dumps({
+            'error': 'forbidden',
+            'error_description': descr
+        }),
+        status=403,
+        mimetype='application/json',
+        headers={
+            "Cache-Control": "no-store",
+            "Pragma": "no-cache"
+        }
+    )
+
+def ok(data):
+    return Response(
+        response=dumps(data),  
+        status=200,  
+        mimetype='application/json' 
+    )
